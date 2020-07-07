69b851e8372ab3f6615712264fafac7ca65e28a2
zhengminghui <zhengminghui@sunline.cn>
2020-07-07 21:28:24 +0800

    日志脱敏消息转换类

---
+++ src/main/java/top/mjava/desensitization/Desensitization.java
@@ -0,0 +1,171 @@
+package top.mjava.desensitization;
+
+/**
+ * 脱敏方法
+ * @author zheng
+ * @date 2020-07-07
+ */
+public class Desensitization {
+
+    public String type(String type,String value){
+        if (value!=null && !value.isEmpty()){
+            String res="";
+            switch (type){
+                case "MOBILE":
+                    res=mobileDesensitization(value);
+                    break;
+                case "EMAIL":
+                    res=emailDesensitization(value);
+                    break;
+                case "BANK_CODE":
+                    res=bakCodeDesensitization(value);
+                    break;
+                case "ID_CARD":
+                    res=idCardDesensitization(value);
+                    break;
+                case "PASSWORD":
+                    res=pwdDesensitization(value);
+                    break;
+                case "NAME":
+                    res=nameDesensitization(value);
+                    break;
+                default:
+                    res=desensitization(value);
+                    break;
+            }
+            return res;
+        }else{
+            return value;
+        }
+    }
+
+    /**
+     * 通用脱敏  保留前三位后三位
+     * 字段长度小于9时  保留后三位
+     * 字段长度小于6 全脱
+     **/
+    public String desensitization(String s){
+        if (s==null || s.isEmpty()){
+            return s;
+        }
+        if (s.length()<9){
+            if (s.length()<6){
+                return "******";
+            }
+            return "******"+s.substring(6);
+        }
+        return s.substring(0,3)+"******"+s.substring(s.length()-3);
+    }
+
+
+    /**
+     * 中国 手机号脱敏
+     * 保留前三位 后四位
+     * */
+    public String mobileDesensitization(String mobile){
+        if (mobile==null || mobile.isEmpty()){
+            return mobile;
+        }
+        if (mobile.length()!=11){
+            return mobile;
+        }
+        return mobile.substring(0,3)+"****"+mobile.substring(mobile.length()-4);
+    }
+
+    /**
+     * 密码脱敏
+     * 全脱
+     * */
+    public String pwdDesensitization(String pwd){
+        if (pwd==null || pwd.isEmpty()){
+            return pwd;
+        }
+        return "********";
+    }
+
+    /**
+     * 身份证脱敏  18位
+     * 保留最后4位
+     * */
+    public String idCardDesensitization(String idCard){
+        if (idCard==null || idCard.isEmpty()){
+            return idCard;
+        }
+        return "**************"+idCard.substring(idCard.length()-4);
+    }
+
+    /**
+     * 邮箱脱敏
+     * 保留 @后面的字符
+     * */
+    public String emailDesensitization(String email){
+        if (email==null || email.isEmpty()){
+            return email;
+        }
+        String[] e=email.split("@");
+        if (e.length!=2){
+            return email;
+        }
+        if (e[0]==null || e[0].isEmpty()){
+            return email;
+        }
+        String pre=this.desensitization(e[0]);
+        return pre+"@"+e[1];
+    }
+
+    /**
+     * 银行卡脱敏
+     * 保留前6位 后4位
+     * */
+    public String bakCodeDesensitization(String idCard){
+        if (idCard==null || idCard.isEmpty()){
+            return idCard;
+        }
+        return replaceBetween(idCard,6,idCard.length()-4,"*");
+    }
+
+    /**
+     * 名字脱敏
+     * 去除姓氏 保留名
+     * */
+    public String nameDesensitization(String name){
+        if (name==null || name.isEmpty()){
+            return name;
+        }
+        return "*"+name.substring(1);
+    }
+
+    /**
+     * 将字符串开始位置到结束位置之间的字符用指定字符替换
+     * @param sourceStr 待处理字符串
+     * @param begin	开始位置
+     * @param end	结束位置
+     * @param replacement 替换字符
+     * @return
+     */
+    private static String replaceBetween(String sourceStr, int begin, int end, String replacement) {
+        if (sourceStr == null) {
+            return "";
+        }
+        if (replacement == null) {
+            replacement = "*";
+        }
+        int replaceLength = end - begin;
+        if (sourceStr.isEmpty() && replaceLength > 0) {
+            StringBuilder sb = new StringBuilder(sourceStr);
+            sb.replace(begin, end, repeat(replacement,replaceLength));
+            return sb.toString();
+        } else {
+            return sourceStr;
+        }
+    }
+
+
+    private static String repeat(String s,int count){
+        StringBuilder sb=new StringBuilder();
+        for (int i=0;i<=count;i++){
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+}
---
+++ src/main/java/top/mjava/desensitization/TopLogMsgConvert.java
@@ -0,0 +1,223 @@
+package top.mjava.desensitization;
+
+import ch.qos.logback.classic.pattern.MessageConverter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONException;
+import com.alibaba.fastjson.JSONObject;
+import org.slf4j.helpers.MessageFormatter;
+import org.springframework.util.ResourceUtils;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Properties;
+
+
+/**
+ *
+ * logback 日志脱敏消息转换类
+ * @author zheng
+ * @date 2020-07-07
+ *
+ */
+public class TopLogMsgConvert extends MessageConverter {
+    private Map<String, LinkedList<String>> relusMap;
+    private boolean init = false;
+    private boolean enable=false;
+    private Desensitization desensitization=new Desensitization();
+    private boolean fileIOException =false;
+    private int maxlength =1048576;
+    private static final String CONFIG_PATH="classpath:logback-desensitization-rule.properties";
+    private static final String MAP_KEY_DELIMITER ="#";
+    private static final String PROPERTIES_KEY_DELIMITER ="/";
+    private static final String PROPERTIES_KEY_VALUE_DELIMITER ="&";
+    private static final String PROPERTIES_KEY_ENABLE="enable";
+    private static final String PROPERTIES_KEY_ENABLE_TRUE="true";
+    private static final String PROPERTIES_KEY_LOG_MAX_LENGTH="log_max_length";
+    private static final String PROPERTIES_KEY_DESENSITIZATION_CLASS_PATH="desensitization_class_path";
+
+    @Override
+    public String convert(ILoggingEvent event){
+        if (!this.init){
+            this.init();
+        }
+        if (this.relusMap!=null && this.enable && !this.fileIOException ){
+            if (event.getFormattedMessage().getBytes().length> maxlength){
+                //超过设定长度 不脱敏 直接返回
+                return super.convert(event);
+            }
+            //开启脱敏
+            return this.convertMsg(event);
+        }
+        return super.convert(event);
+    }
+
+    /**
+     * 初始化配置
+     * */
+    private void init() {
+        synchronized (TopLogMsgConvert.class) {
+            this.relusMap = new HashMap<>();
+            try {
+                File file=ResourceUtils.getFile(CONFIG_PATH);
+                InputStream in = new BufferedInputStream(new FileInputStream(file));
+                Properties p = new Properties();
+                p.load(in);
+                int keyNum=0;
+                for (Object k:p.keySet()){
+                    keyNum++;
+                    if (PROPERTIES_KEY_ENABLE.equals(k)){
+                        if (PROPERTIES_KEY_ENABLE_TRUE.equals(p.getProperty(k.toString()))){
+                            this.enable=true;
+                        }
+                    }else if(PROPERTIES_KEY_LOG_MAX_LENGTH.equals(k)){
+                        if (!p.getProperty(k.toString()).isEmpty()){
+                            int length=this.getByteLength(p.getProperty(k.toString()));
+                            if (length>0){
+                                this.maxlength=length;
+                            }
+                        }
+                    }else if (PROPERTIES_KEY_DESENSITIZATION_CLASS_PATH.equals(k)){
+                        if (!p.getProperty(k.toString()).isEmpty()){
+                            try {
+                                Class<?> c=Class.forName(p.getProperty(k.toString()));
+                                this.desensitization= (Desensitization) c.newInstance();
+                            }catch (Exception e){
+                                System.out.println("\033[31;4m" +e.getMessage()+"\n" +e+ "\033[0m");
+                            }
+                        }
+                    }else{
+                        String key=k.toString();
+                        String[] kv=key.split(PROPERTIES_KEY_VALUE_DELIMITER);
+                        if (kv.length==2){
+                            String k2=keyNum+MAP_KEY_DELIMITER+kv[1];
+                            String[] ks=kv[0].split(PROPERTIES_KEY_DELIMITER);
+                            LinkedList<String> linkedList=new LinkedList<>();
+                            for (String value:ks){
+                                if (!value.isEmpty()){
+                                    linkedList.addLast(value);
+                                }
+                            }
+                            this.relusMap.put(k2,linkedList);
+                        }else if (kv.length==1){
+                            String k2=keyNum+MAP_KEY_DELIMITER;
+                            String[] ks=kv[0].split(PROPERTIES_KEY_DELIMITER);
+                            LinkedList<String> linkedList=new LinkedList<>();
+                            for (String value:ks){
+                                if (!value.isEmpty()){
+                                    linkedList.addLast(value);
+                                }
+                            }
+                            this.relusMap.put(k2,linkedList);
+                        }
+                    }
+                }
+                this.init = true;
+            }catch (Exception e){
+                System.out.println("\033[31;4m" + "Abnormal loading desensitization configuration file：\n" +e+ "\033[0m");
+                this.fileIOException =true;
+                this.init = true;
+            }
+        }
+    }
+
+    private String convertMsg(ILoggingEvent event){
+        /**
+         * 获取mag
+         * eg：log.info("测试数据：{}"，arg)
+         * 获取“测试数据：{}”
+         * */
+        String msg=event.getMessage();
+
+        /**
+         * 遍历所有参数
+         * eg：log.info("测试数据：{}",arg)
+         * 可以获取到arg数据
+         * */
+        Object[] args=event.getArgumentArray();
+        if (args!=null && args.length>0){
+            for (int i=0;i<args.length;i++){
+                String jsonString=args[i].toString();
+                if ("{".equals(jsonString.substring(0,1)) &&
+                        "}".equals(jsonString.substring(jsonString.length()-1))){
+                    try {
+                        JSONObject arg= JSON.parseObject(jsonString);
+                        LinkedList<String> l=new LinkedList<>();
+                        for (String k:relusMap.keySet()){
+                            String[] k2=k.split(MAP_KEY_DELIMITER);
+                            if (k2.length==1){
+                                this.jsonDesensitization(arg,relusMap.get(k),"default");
+                            }else if (k2.length==2){
+                                this.jsonDesensitization(arg,relusMap.get(k),k2[1]);
+                            }
+                        }
+                        args[i]=arg;
+                    }catch (JSONException e){
+                        System.out.println("\033[31;4m" + e.getMessage()+"\n" +e+ "\033[0m");
+                    }
+                }
+            }
+        }
+        return MessageFormatter.arrayFormat(msg, args).getMessage();
+    }
+
+    /**
+     * json层级循环脱敏
+     * @param json json对象
+     * @param list 层级列表
+     * @param type 脱敏类型
+     * */
+    private  void jsonDesensitization(JSONObject json, LinkedList<String> list,String type) {
+        if (json.get(list.getFirst()) instanceof JSONObject){
+            JSONObject jsonObject= (JSONObject) json.get(list.getFirst());
+            list.removeFirst();
+            if (list.size()>0){
+                this.jsonDesensitization(jsonObject,list,type);
+            }
+        }else if (json.get(list.getFirst()) instanceof String){
+            String v=json.get(list.getFirst()).toString();
+            v=desensitization.type(type,v);
+            json.put(list.getFirst(),v);
+        }
+    }
+
+    /**
+     * 字节换算
+     * 8bit(位)=1Byte(字节)
+     * 1024Byte(字节)=1KB
+     * 1024KB=1MB
+     * 1024MB=1GB
+     * 1024GB=1TB
+     * */
+    private int getByteLength(String l){
+        if (l==null || l.isEmpty()){
+            return -1;
+        }
+        int res=-1;
+        try {
+            res=Integer.parseInt(l.substring(0,l.length()-1));
+        }catch (Exception e){
+            System.out.println("\033[31;4m" + "Type conversion error：\n" +e+ "\033[0m");
+            return -1;
+        }
+        switch (l.substring(l.length()-1)){
+            case "b":
+                break;
+            case "k":
+                res=res*1024;
+                break;
+            case "m":
+                res=res*1024*1024;
+                break;
+            default:
+                break;
+        }
+        return res;
+    }
+
+}
