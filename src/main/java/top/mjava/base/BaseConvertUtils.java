4276a3f9a48b584b1b5e6c9d5acbc4c8be724642
zhengminghui <zhengminghui99@gmail.com>
2020-07-29 15:19:23 +0800

    Optimized base conversion

--- src/main/java/top/mjava/base/BaseConvertUtils.java
+++ src/main/java/top/mjava/base/BaseConvertUtils.java
@@ -14,23 +14,71 @@ import java.util.Map;
  * @date 2020/7/27
  **/
 public class BaseConvertUtils {
-    private static final char[] BASE_64_ARRAY = { 'z','x','c','v','b','n','m','a','s','d','f','g','h','j','k','l','q','w',
-    'e','r','t','y','u','i','o','p','1','2','3','4','5','6','-','7','8','9','Z','X','C','V','B','N','M','A','S','D','F',
-    'G','H','J','K','L','Q','W','E','R','T','Y','U','I','O','P','0','+'};
+    private final static int BASE_16_POWER=4;
+    private final static int BASE_32_POWER=5;
+    private final static int BASE_64_POWER=6;
 
-    private static final char[] BASE_64_URL_ARRAY = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N',
-    'O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n',
-            'o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','-','_'};
 
-    private static final char[] HEX_ARRAY = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 
-    private static final char[] BASE_32_ARRAY = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E'
-            ,'F','G','H','J','K','M','N','P','Q','R','S','T','V','W','X','Y','Z'};
+    /**
+     * 10进制转16进制
+     * */
+    public static String base10ToHex(Long val){
+        return base10ToAll(val,BaseDictionary.HEX_ARRAY.getChars(),BASE_16_POWER);
+    }
+
+    /**
+     * 16进制转10进制
+     * */
+    public static Long hexToBase10(String val){
+        return allToBase10(val,BaseDictionary.HEX_ARRAY.getChars(),BASE_16_POWER);
+    }
+
+    /**
+     * 10进制转32进制
+     * */
+    public static String base10ToBase32(Long val){
+        return base10ToAll(val,BaseDictionary.BASE_32_ARRAY.getChars(),BASE_32_POWER);
+    }
+
+    /**
+     * 32进制转10进制
+     * */
+    public static Long base32ToBase10(String val){
+        return allToBase10(val,BaseDictionary.BASE_32_ARRAY.getChars(),BASE_32_POWER);
+    }
 
     /**
      * 10进制转64进制
      * */
     public static String base10ToBase64(Long val){
+        return base10ToAll(val,BaseDictionary.BASE_64_URL_ARRAY.getChars(),BASE_64_POWER);
+    }
+
+    /**
+     * 64进制转10进制
+     * */
+    public static Long base64ToBase10(String val){
+        return allToBase10(val,BaseDictionary.BASE_64_URL_ARRAY.getChars(),BASE_64_POWER);
+    }
+
+    private static Long allToBase10(String val,char[] chars,int power){
+        long result=0L;
+        char[] c=val.toCharArray();
+        Map<Character,Integer> map=new HashMap<>();
+        for (int i=0;i<chars.length;i++){
+            map.put(chars[i],i);
+        }
+        for (int j=0;j<c.length;j++){
+            // eg: 64转10进制 从右到左 从64的0次方开始
+            // eg: (eq)64 -> (e*64^1+q*64^0)10
+            // 然后根据字典位置 e在字典下标为2 q在字典下标为0 -> (2*64^1+0*64^0)10=128
+            result += (long) map.get(c[j])<<(power*(c.length-j-1));
+        }
+        return result;
+    }
+
+    private static String base10ToAll(Long val,char[] chars,int power){
         Long l1=val;
         long y;
         StringBuilder result=new StringBuilder();
@@ -39,9 +87,9 @@ public class BaseConvertUtils {
         LinkedList<Character> list=new LinkedList<>();
         while (l1 > 0){
             //取余数运算
-            y=l1-((l1>>6)<<6);
-            list.push(BASE_64_URL_ARRAY[(int) y]);
-            l1=l1>>6;
+            y=l1-((l1>>power)<<power);
+            list.push(chars[(int) y]);
+            l1=l1>>power;
         }
         //从栈头取出数据
         while (!list.isEmpty()){
@@ -50,96 +98,15 @@ public class BaseConvertUtils {
         return result.toString();
     }
 
-    /**
-     * 64进制转10进制
-     * */
-    public static Long base64ToBase10(String val){
-        char[] c=val.toCharArray();
-        long result=0;
-        // 将字典哈希
-        Map<Character,Integer> map=new HashMap<>();
-        for (int i=0;i<BASE_64_URL_ARRAY.length;i++){
-            map.put(BASE_64_URL_ARRAY[i],i);
-        }
-        for (int i=0;i<c.length;i++){
-            // 64转10进制 从右到左 从64的0次方开始
-            // eq: (eq)64 -> (e*64^1+q*64^0)10
-            // 然后根据字典位置 e在字典下标为2 q在字典下标为0 -> (2*64^1+0*64^0)10=128
-            result += (long) map.get(c[i])<<(6*(c.length-1-i));
-        }
-        return result;
-    }
+    @Test
+    public void test(){
+        System.out.println(base10ToBase64(6644422222L));
+        System.out.println(base64ToBase10(base10ToBase64(6644422222L)));
 
+        System.out.println(base10ToBase32(123123123445567L));
+        System.out.println(base32ToBase10(base10ToBase32(123123123445567L)));
 
-    /**
-     * 10进制转16进制
-     * */
-    public static String base10ToHex(Long val){
-        long n=val;
-        long y=0;
-        StringBuilder result=new StringBuilder();
-        LinkedList<Character> list=new LinkedList<>();
-        while (n>0){
-            y=n-((n>>4)<<4);
-            list.push(HEX_ARRAY[(int) y]);
-            n=n>>4;
-        }
-        //从栈头取出数据
-        while (!list.isEmpty()){
-            result.append(list.pop());
-        }
-        return result.toString();
-    }
-
-    /**
-     * 16进制转10进制
-     * */
-    public static Long hexToBase10(String val){
-        long result=0;
-        char[] c=val.toCharArray();
-        Map<Character,Integer> map=new HashMap<>();
-        for (int i=0;i<HEX_ARRAY.length;i++){
-            map.put(HEX_ARRAY[i],i);
-        }
-        for (int j=0;j<c.length;j++){
-            result += (long) map.get(c[j])<<(4*(c.length-j-1));
-        }
-        return result;
-    }
-
-    /**
-     * 10进制转32进制
-     * */
-    public static String base10ToBase32(Long val){
-        long n=val;
-        long y=0L;
-        StringBuilder result=new StringBuilder();
-        LinkedList<Character> list=new LinkedList<>();
-        while (n>0){
-            y=n-((n>>5)<<5);
-            list.push(BASE_32_ARRAY[(int) y]);
-            n=n>>5;
-        }
-        //从栈头取出数据
-        while (!list.isEmpty()){
-            result.append(list.pop());
-        }
-        return result.toString();
-    }
-
-    /**
-     * 32进制转10进制
-     * */
-    public static Long base32ToBase10(String val){
-        long result=0L;
-        char[] c=val.toCharArray();
-        Map<Character,Integer> map=new HashMap<>();
-        for (int i=0;i<BASE_32_ARRAY.length;i++){
-            map.put(BASE_32_ARRAY[i],i);
-        }
-        for (int j=0;j<c.length;j++){
-            result += (long) map.get(c[j])<<(5*(c.length-j-1));
-        }
-        return result;
+        System.out.println(base10ToHex(9898989887L));
+        System.out.println(hexToBase10(base10ToHex(9898989887L)));
     }
 }
---
+++ src/main/java/top/mjava/base/BaseDictionary.java
@@ -0,0 +1,54 @@
+package top.mjava.base;
+
+/**
+ * 进制转换字典
+ * @author zmh
+ * @version 1.0
+ * @date 2020/7/29
+ **/
+public enum BaseDictionary {
+
+    BASE_64_ARRAY("64进制","64进制乱序字典",new char[]{'z','x','c','v','b','n','m','a','s','d','f','g','h','j','k','l','q','w',
+            'e','r','t','y','u','i','o','p','1','2','3','4','5','6','-','7','8','9','Z','X','C','V','B','N','M','A','S','D','F',
+            'G','H','J','K','L','Q','W','E','R','T','Y','U','I','O','P','0','+'}),
+    BASE_64_URL_ARRAY("64进制","64进制url标准字典",new char[]{'A','B','C','D','E','F','G','H','I','J','K','L','M','N',
+            'O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n',
+            'o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','-','_'}),
+    HEX_ARRAY("16进制","16进制标准字典",new char[]{'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'}),
+    BASE_32_ARRAY("32进制","32进制标准字典",new char[]{'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E'
+            ,'F','G','H','J','K','M','N','P','Q','R','S','T','V','W','X','Y','Z'});
+
+    private String name;
+    private String desc;
+    private char[] chars;
+
+    BaseDictionary(String name, String desc, char[] chars) {
+        this.name = name;
+        this.desc = desc;
+        this.chars = chars;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public char[] getChars() {
+        return chars;
+    }
+
+    public void setChars(char[] chars) {
+        this.chars = chars;
+    }
+}
